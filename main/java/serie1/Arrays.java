ec70d7c3f3daba541056523dbac21fc83aa997ed
Filipe Azevedo <filipe.kimura@gmail.com>
2014-10-17 20:51:07 +0100

    Initial COmmit

---
+++ main/java/serie1/Arrays.java
@@ -0,0 +1,21 @@
+package serie1;
+
+public class Arrays {
+
+	public static int countEquals(int[] v1, int l1, int r1, int[] v2, int l2, int r2){
+		throw new UnsupportedOperationException();
+	}
+	
+	public static int hIndex(int[] v){
+		throw new UnsupportedOperationException();
+	}
+
+	
+	public static int deleteMin(int[] maxHeap, int sizeHeap){
+		throw new UnsupportedOperationException();
+	}
+	
+	public static void sortIPv4Addresses(String[] v, int l, int r) {
+		throw new UnsupportedOperationException();
+	}
+}
