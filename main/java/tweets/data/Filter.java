edad53d36532a6badb7784ec26583d82f183a989
danny81m <danny81m@gmail.com>
2014-10-26 19:31:14 +0000

    Filter corrigido.
    Tweet - criados metodos boolean para HasHashTag e IsEndOffile

--- main/java/tweets/data/Filter.java
+++ main/java/tweets/data/Filter.java
@@ -10,51 +10,61 @@ import tweets.model.Tweet;
  * pede ao parser novos tweets, filtra-os usando field filters.
  */
 public class Filter {
-	
-	String twtFile;
-	//private Parser parsedObg = new Parser();
-	
-	
-	private ArrayList<FieldFilter> filterAL  = new ArrayList<FieldFilter>();
-	
+
 	/**
-	 * Construtor sem filtros
+	 * Instancia de parser associada ao construtor
+	 */
+	Parser ps;
+
+	/**
+	 * Constroi uma arraylist de filtros a serem usados nas pesquisas
+	 */
+	private ArrayList<FieldFilter> filterAL  = new ArrayList<FieldFilter>();
+
+	/**
+	 * Construtoi um filtro para trabalhar com o ficheiro especificado
+	 * constroi um parser associado a este filtro.
 	 */
 	public Filter(String twtFile){
-		this.twtFile=twtFile;
+		ps = new Parser(twtFile);
 	}
-	
+
 	/**
 	 * Adiciona filtros à lista de filtros Arraylist
 	 */
 	public void addFilter(FieldFilter f){
 		filterAL.add(f);
 	}
+
 	
-	public Tweet findOne(){
-		Parser ps = new Parser(twtFile);
-		Tweet tw;
-		while (true){
-			tw= findSingle(ps);
-			if (tw!= null)	return tw;
-			break;
+
+	/**
+	 * O metodo Parser.parseTweet() devolve o proximo Tweet
+	 * É feito um for para validar se esse tweet contem todas as HashTags.
+	 * Retorna o tweet se sim, null se não encontrada correspondencia. 
+	 * Nota: pressupoe que ha um tweet especifico se o ficheiro chegar ao fim.
+	 */
+	public Tweet findNext(){
+		Tweet tw = null ;
+		boolean match = false;
+		int matchCount = 0;
+
+		while ( match == false || tw.isEndOfFile() ){
+			tw = ps.parseTweet();
+			matchCount = 0;
+			for(int i=0; i<filterAL.size(); i++){	
+				if ( tw.hasHashTag(filterAL.get(i)) ) matchCount++;
+				else break;
 			}
-		return null;
-	}
-	
-	
-	private Tweet findSingle(Parser ps){
-		Tweet tw = ps.parseTweet();
-		for(int i=0; i<filterAL.size(); i++){	
-			if ( ! tw.hasHashtags(filterAL.get(i)) ) return null;
+			if (matchCount == filterAL.size()) 	match = true;
 		}
-		return tw;
-		
+		if (match) return tw;
+		else return null;
 	}
 
-	public HashTags getHashTags(String s){
-		return Parser.getHashTags(s);
-	}
-	
-	
+//	public HashTags getHashTags(String s){
+//		return Parser.getHashTags(s);
+//	}
+
+
 }
