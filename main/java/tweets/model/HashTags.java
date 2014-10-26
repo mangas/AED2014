1a3104f6c2a2e6681687e16d9218b4e03b2389e7
Filipe Azevedo <filipe.kimura@gmail.com>
2014-10-26 17:44:41 +0000

    - new Hastags

---
+++ main/java/tweets/model/HashTags.java
@@ -0,0 +1,31 @@
+package tweets.model;
+
+/**
+ * Created by fa on 26/10/14.
+ */
+public class HashTags {
+
+    private  String[] hashtags;
+
+    public HashTags(String[] hashtags) {
+        this.hashtags=hashtags;
+    }
+
+    public String[] getHashtags() {
+        return this.hashtags;
+    }
+
+    public boolean contains(String hashtag) {
+
+        boolean found = false;
+
+        for (String s : hashtags)
+            if (s.equalsIgnoreCase(hashtag)) {
+                found = true;
+                break;
+            }
+
+        return found;
+    }
+
+}
