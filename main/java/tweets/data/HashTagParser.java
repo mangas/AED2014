b2b281af3b60cbc3866ff0e1131726e542ca09e8
Filipe Azevedo <filipe.kimura@gmail.com>
2014-10-26 17:02:03 +0000

    - new Hastags
    - Parser

---
+++ main/java/tweets/data/HashTagParser.java
@@ -0,0 +1,12 @@
+package tweets.data;
+
+/**
+ * Created by fa on 26/10/14.
+ */
+public class HashTagParser extends AbstractParser<String> {
+
+    @Override
+    public String parseOne(String line) {
+        return null;
+    }
+}
