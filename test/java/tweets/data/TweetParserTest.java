f2a4f391c351b1e869bd31a3a7f23cb83be73c95
Filipe Azevedo <mangas>
2014-11-02 14:39:20 +0000

    IDE specific

--- test/java/tweets/data/TweetParserTest.java
+++ test/java/tweets/data/TweetParserTest.java
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-package tweets.data;
-
-import org.junit.Test;
-import tweets.model.Tweet;
-
-import static org.junit.Assert.assertEquals;
-
-/**
- * Created by fa on 02/11/14.
- */
-
-public class TweetParserTest {
-
-
-    @Test
-    public static void parseTweetTest() {
-        final String tweet1 = "{\"created_at\": \"2014-07-06T18:08:28\", \"hashtags\": [\"worldcup\"], \"id\": 485847765772021760, \"uid\": 34191594}"
-        final String tweet2 = "{\"created_at\": \"2014-07-06T18:08:28\", \"hashtags\": [\"Schweinsteiger\", \"Neuer\"], \"id\": 485847766384775168, \"uid\": 2566075189}";
-
-        Tweet tw = new TweetParser().parseOne(tweet1);
-
-
-
-        Tweet tw2 = new TweetParser().parseOne(tweet2);
-    }
-}
-=======
 package tweets.data;
 
 import org.junit.Test;
@@ -55,4 +27,3 @@ public class TweetParserTest {
 
     }
 }
->>>>>>> FETCH_HEAD
