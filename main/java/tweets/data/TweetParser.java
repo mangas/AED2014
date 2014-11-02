5e7c0c5938d75ca389bc16d3575a572f4be787d4
Filipe Azevedo <mangas>
2014-11-02 14:19:43 +0000

    TweetParser, HashTags
    further TweetParser development

--- main/java/tweets/data/TweetParser.java
+++ main/java/tweets/data/TweetParser.java
@@ -1,14 +1,28 @@
 package tweets.data;
 
+import tweets.model.HashTags;
 import tweets.model.Tweet;
 
 /**
  * Created by fa on 26/10/14.
  */
+
 public class TweetParser extends AbstractParser<Tweet> {
 
     @Override
     public Tweet parseOne(String line) {
+
+        HashTags ht = new HashTags();
+
+        String[] fields = line.split(":");
+
+
+
+        for (int i=0;i<fields.length;i++) {
+            System.out.println(fields[i]);
+        }
+
+
         return null;
     }
 }
--- main/java/tweets/model/HashTags.java
+++ main/java/tweets/model/HashTags.java
@@ -7,12 +7,22 @@ public class HashTags {
 
     private  String[] hashtags;
 
+    public HashTags() {
+    }
+
     public HashTags(String[] hashtags) {
         this.hashtags=hashtags;
     }
 
     public String[] getHashtags() {
         return this.hashtags;
+    }
+
+    public boolean equals(HashTags ht) {
+        for (String s : ht.getHashtags())
+            if (!contains(s)) return false;
+
+        return true;
     }
 
     public boolean contains(String hashtag) {
