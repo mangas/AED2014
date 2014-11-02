36a25622e3e0e918dc4a3da38fbf6e5facac78f3
danny81m <danny81m@gmail.com>
2014-11-02 14:44:45 +0000

    filter

--- main/java/tweets/data/Filter.java
+++ main/java/tweets/data/Filter.java
@@ -5,25 +5,33 @@ import java.util.ArrayList;
 import tweets.model.Tweet;
 
 /**
- * Created by fa on 26/10/14.
  * esta classe é o engine de filtragem.
  * pede ao parser novos tweets, filtra-os usando field filters.
+ * 
+ * Uso: 1º Construir uma instancia com o ficheiro fonte de tweets como argumento
+ * 		2º adicionar os filtros com o metodo addFilter
+ * 		3º filtrar com o filterOne ou com o FilterAll
  */
 public class Filter {
-	
-	String twtFile;
-	//private Parser parsedObg = new Parser();
-	
-	
-	private ArrayList<FieldFilter> filterAL  = new ArrayList<FieldFilter>();
-	
+
 	/**
-	 * Construtor sem filtros
+	 * Instancia de parser associada ao construtor
+	 */
+	Parser ps;
+
+	/**
+	 * Constroi uma arraylist de filtros a serem usados nas pesquisas
+	 */
+	private ArrayList<FieldFilter> filterAL  = new ArrayList<FieldFilter>();
+
+	/**
+	 * Construtoi um filtro para trabalhar com o ficheiro especificado
+	 * constroi um parser associado a este filtro.
 	 */
 	public Filter(String twtFile){
-		this.twtFile=twtFile;
+		ps = new Parser(twtFile);
 	}
-	
+
 	/**
 	 * Adiciona filtros à lista de filtros Arraylist
 	 */
@@ -31,30 +39,44 @@ public class Filter {
 		filterAL.add(f);
 	}
 	
-	public Tweet findOne(){
-		Parser ps = new Parser(twtFile);
-		Tweet tw;
-		while (true){
-			tw= findSingle(ps);
-			if (tw!= null)	return tw;
-			break;
+	/**
+	 * O metodo Parser.parseTweet() devolve o proximo Tweet
+	 * É feito um for para validar se esse tweet contem todas as HashTags.
+	 * Retorna o tweet se sim, null se não encontrada correspondencia. 
+	 * Nota: pressupoe que recebe null se o ficheiro chegar ao fim.
+	 */
+	public Tweet filterOne(){
+		Tweet tw = null ;
+		boolean match = false;
+		int matchCount = 0;
+
+		while ( match == false || tw == null ){
+			tw = ps.parseTweet();
+			matchCount = 0;
+			for(int i=0; i<filterAL.size(); i++){	
+				if ( tw.hasHashTag(filterAL.get(i)) ) matchCount++;
+				else break;
 			}
-		return null;
+			if (matchCount == filterAL.size()) 	match = true;
+		}
+		if (match) return tw;
+		else return null;
 	}
 	
-	
-	private Tweet findSingle(Parser ps){
-		Tweet tw = ps.parseTweet();
-		for(int i=0; i<filterAL.size(); i++){	
-			if ( ! tw.hasHashtags(filterAL.get(i)) ) return null;
-		}
-		return tw;
-		
+	/**
+	 * Filtra todos os elementos do ficheiro fonte e retorna um array de tweets
+	 */
+	public Tweet[] filterAll(){
+		ArrayList<Tweet> tweets  = new ArrayList<Tweet>(500);
+		Tweet tw = filterOne();
+		while (tw != null) 
+			tweets.add(tw);
+		return (Tweet[]) tweets.toArray();
 	}
 
-	public HashTags getHashTags(String s){
-		return Parser.getHashTags(s);
-	}
-	
-	
+//	public HashTags getHashTags(String s){
+//		return Parser.getHashTags(s);
+//	}
+
+
 }
